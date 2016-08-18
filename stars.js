59e7bfde77ed5c58ca40cefe20986bb770dde7dd
Spencer Harrison <spencerharrison@theexperiment.io>
2016-08-18 11:17:01 -0400

    added posters lots of updates

--- stars.js
+++ stars.js
@@ -1,14 +1,16 @@
-var stars = {};
-
 function setup() {
-  var canvas = createCanvas(1280, 400);
+  var canvas = createCanvas(800,600);
+  canvas.position(width/2,height/2);
+  canvas.class("stars")
   frameRate(4);
   canvas.parent('starfield');
 };
 
+
+
 function draw() {
   background(25); // or (R, G, B)
-
+  ellipse(width/2, height/2, 100, 100);
   for (var i=0; i<100; i++) {
     ellipse(Math.random() * 1280, Math.random() * 1000, 3, 3);
   };
@@ -16,3 +18,5 @@ function draw() {
     stars[k].render();
   }
 };
+
+var stars = {};
